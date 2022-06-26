0958eb41a835871e79207224dd41e26083b58180
Jessica <jessicanevesmachado@gmail.com>
2022-06-26 11:27:46 -0300

    add server fake using json server

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+console.log("Servidor rodando na porta:" + port);
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
